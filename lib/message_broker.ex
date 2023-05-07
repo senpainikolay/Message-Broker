90ca97c6085509e927820f1d19bbe79b7b886514
senpainikolay <nicolae32gherman@gmail.com>
2023-05-07 10:50:57 +0300

    add TCP connection handling

--- lib/message_broker.ex
+++ lib/message_broker.ex
@@ -1,5 +1,19 @@
-defmodule MessageBroker do
-  def hello do
-    :world
+defmodule TcpServer.ConnectionSupervisor do
+  use Supervisor
+
+  def start() do
+    cmd1 = ["GET", "CREATE", "PUBLISH"]
+    cmd2 = ["GET", "SUBSCRIBE","UNSUBSCRIBE"]
+    children =
+      [
+      {Task.Supervisor, name: PublisherConnectionsSupervisor},
+      {Task.Supervisor, name: ConsumerConnectionsSupervisor},
+      Supervisor.child_spec({Task, fn -> TcpServer.accept(4000,PublisherConnectionsSupervisor,cmd1) end}, restart: :permanent, id: :PublisherServer ),
+      Supervisor.child_spec({Task, fn -> TcpServer.accept(4001,ConsumerConnectionsSupervisor,cmd2) end}, restart: :permanent, id: :ConsumerServer)
+      ]
+    Supervisor.start_link(__MODULE__, children, name: __MODULE__)
+  end
+  def init(children) do
+    Supervisor.init(children, strategy: :one_for_one,  max_restarts: 10, max_seconds: 10 )
   end
 end
